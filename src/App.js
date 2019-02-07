9518552122e2f5efa6a10fd36c2252f0f128e105
karim_mokhtar <kmokhtar@abuerdan.com>
2019-02-07 16:23:02 +0200

    Edit the main page design

--- src/App.js
+++ src/App.js
@@ -7,11 +7,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Welcome to Cryptocurrency website
-        </header>
-        <Today />
-        <Past /> 
+        <div className="topheader">
+            <header className="container">
+                <nav className="navbar">
+                    <div className="navbar-brand">
+                        <span className="navbar-item">Cryptocurency Prices</span>
+                    </div>
+                    <div className="navbar-end">
+                        <a className="navbar-item" href="#" target="_blank" rel="noopener noreferrer">Cryptoprice.com</a>
+                    </div>
+                </nav>
+            </header>
+        </div>
+         <section className="results--section">
+            <div className="container">
+                <h1>CryptoPrice is a realtime price information about<br></br> BTC, ETH and LTC.</h1>
+            </div>
+            <div className="results--section__inner">
+                <Today />
+                <Past /> 
+            </div>
+        </section>
+        
       </div>
     );
   }
