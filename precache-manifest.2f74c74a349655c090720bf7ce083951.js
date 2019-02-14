6e1c0543cc634763f3b7f0a82a3f6637ace754e9
karimmokhtar <karimmokhtar28@gmail.com>
2019-02-14 22:35:02 +0200

    Updates

---
+++ precache-manifest.2f74c74a349655c090720bf7ce083951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CryptoCurrencyPrices-PWA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af89b3b92993b2250bb6",
+    "url": "/CryptoCurrencyPrices-PWA/static/js/runtime~main.af89b3b9.js"
+  },
+  {
+    "revision": "d3a7b8d562f12b2e8554",
+    "url": "/CryptoCurrencyPrices-PWA/static/js/main.d3a7b8d5.chunk.js"
+  },
+  {
+    "revision": "711f10e5a9f5b1fba428",
+    "url": "/CryptoCurrencyPrices-PWA/static/js/1.711f10e5.chunk.js"
+  },
+  {
+    "revision": "d3a7b8d562f12b2e8554",
+    "url": "/CryptoCurrencyPrices-PWA/static/css/main.c2317baf.chunk.css"
+  },
+  {
+    "revision": "7a1cf36bb2dd8f5857a56f9cc9d4f964",
+    "url": "/CryptoCurrencyPrices-PWA/index.html"
+  }
+];
